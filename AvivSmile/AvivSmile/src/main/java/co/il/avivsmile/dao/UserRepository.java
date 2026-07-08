fcb794ca4d08f84b862dcb869c716d0c2c472895
Nikita <nikolaevnik13@gmail.com>
2026-07-08 14:35:41 +0300

    refactoring

--- AvivSmile/AvivSmile/src/main/java/co/il/avivsmile/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package co.il.avivsmile.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.il.avivsmile.model.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-}
